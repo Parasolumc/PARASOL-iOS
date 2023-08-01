24f471d02fabaca729e33d7182426e32a82839f7
imdanaa <jangs1920@gmail.com>
2023-08-01 16:21:07 +0900

    [feature3] 사용자 홈 화면 구성

--- PARASOL/PARASOL/Scenes/Home/UserHome/UserVC.swift
+++ PARASOL/PARASOL/Scenes/Home/UserHome/UserVC.swift
@@ -8,21 +8,270 @@
 import UIKit
 
 class UserVC: UIViewController {
+    // MARK: - Properties
+    // 변수 및 상수, IBOutlet
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    // 화면 사이즈
+    var bounds = UIScreen.main.bounds
+    lazy var screenWidth = bounds.size.width //화면 너비
+    lazy var screenHeight = bounds.size.height //화면 높이
+    
+    // toolbar 사이즈
+    lazy var toolbarBounds = self.navigationController!.navigationBar.bounds
+    lazy var toolbarHeight = toolbarBounds.height
+    
+    // 대여가능한 우산의 개수
+    var umbrellaNum = 9
+    
+    // MARK: [UI components]
+    let toolbar = UIToolbar()
+    // Toolbar items
+    lazy var menuBarItem = UIBarButtonItem.menuButton(self, action: #selector(testAc), imageName: "menu", size: 24, color: "black")
+    lazy var selectHomeBarItem = UIBarButtonItem.menuButton(self, action: #selector(testAc), imageName: "select_home", size: 24, color: "main")
+    lazy var myPageBarItem = UIBarButtonItem.menuButton(self, action: #selector(testAc), imageName: "mypage", size: 24, color: "black")
+    lazy var flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
+    lazy var toolItems = [self.flexibleSpace, self.menuBarItem, self.flexibleSpace, self.selectHomeBarItem, self.flexibleSpace, self.myPageBarItem, self.flexibleSpace]
+
+    @objc func testAc() {
+        let rentalVC = Rental_ReturnVC()
+        rentalVC.nowFun = "Rental"
+        self.navigationController?.pushViewController(rentalVC, animated: true)
     }
     
+    // 검색 바 요소들
+    let searchbar: UISearchBar = {
+        let searchbar = UISearchBar()
+        searchbar.tintColor = .blue
+        
+        return searchbar
+    }()
+    
+    // 매장 정보 화면 요소들
+    lazy var introView: UIView = {
+       let view = UIView()
+        
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .white
+        view.layer.cornerRadius = 38
+        view.layer.maskedCorners = CACornerMask(arrayLiteral: [.layerMinXMinYCorner, .layerMaxXMinYCorner])
+        view.widthAnchor.constraint(equalToConstant: screenWidth).isActive = true
+        view.heightAnchor.constraint(equalToConstant: 300).isActive = true
+        
+        return view
+    }()
+    
+    var nameLabel: UILabel = {
+        let label = UILabel()
+        
+        label.text = "상점명"
+        label.font = .boldSystemFont(ofSize: 18)
+        label.textColor = .black
+        return label
+    }()
+    
+    let findLoadButton:UIButton = {
+        let button = UIButton()
+        
+        button.setImage(UIImage(named: "find_load"), for: .normal)
+        button.setDimensions(height: 45, width: 45)
+        button.contentVerticalAlignment = .fill
+        button.contentHorizontalAlignment = .fill
+        button.layer.cornerRadius = 45 / 2
+        button.clipsToBounds = true
+        
+        return button
+    }()
+    
+    var addressLabel: UILabel = {
+        let label = UILabel()
+        
+        label.text = "서울 서대문구 이화여대길 77"
+        label.font = .systemFont(ofSize: 16)
+        label.textColor = .black
+        return label
+    }()
+    
+    var isOpenLabel: UILabel = {
+        let label = UILabel()
+        
+        label.text = "영업종료"
+        label.font = .boldSystemFont(ofSize: 16)
+        label.textColor = .black
+        
+        return label
+    }()
+    
+    var timeLabel: UILabel = {
+        let label = UILabel()
+        
+        label.text = "09:00에 영업 시작"
+        label.font = .systemFont(ofSize: 16)
+        label.textColor = .black
+        return label
+    }()
+    
+    lazy var storeTimeHStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [self.isOpenLabel, self.timeLabel])
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.alignment = .center
+        stackView.distribution = .equalSpacing
+        stackView.spacing = 10
 
-    /*
-    // MARK: - Navigation
+        return stackView
+    }()
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    lazy var vStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [self.nameLabel, self.addressLabel, self.storeTimeHStackView])
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.alignment = .leading
+        stackView.distribution = .fillProportionally
+        stackView.spacing = 10
+        
+        stackView.setDimensions(height: 80, width: (screenWidth - 74))
+        return stackView
+    }()
+    
+    // 대여 가능 우산 개수 관련 요소들 생성
+    var tag1Label: UILabel = {
+        var label = UILabel()
+        
+        label.text = "대여가능 우산"
+        label.font = .boldSystemFont(ofSize: 16)
+        label.textColor = .black
+        return label
+    }()
+    
+    let umbrellaImage: UIImageView = {
+            let imageView = UIImageView()
+        
+            imageView.image = UIImage(named: "umbrella")
+        
+            return imageView
+    }()
+    
+    lazy var tag2Label: UILabel = {
+        var label = UILabel()
+        
+        label.text = ": " + String(self.umbrellaNum) + "개"
+        label.font = .boldSystemFont(ofSize: 16)
+        label.textColor = .black
+        return label
+    }()
+    
+    lazy var umbrellaHStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [self.tag1Label, self.umbrellaImage, self.tag2Label])
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.alignment = .center
+        stackView.distribution = .equalSpacing
+        stackView.spacing = 6
+
+        return stackView
+    }()
+    
+    // 대여/반납 버튼 생성
+    lazy var rentalButton: UIButton = {
+        let button = UIButton()
+        
+        button.setTitle("대여", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.setDimensions(height: 54, width: 126)
+        button.layer.cornerRadius = 20
+        button.backgroundColor = UIColor(named: "main")
+        
+        let goToRentalVCAction = UIAction { _ in
+            let rentalVC = Rental_ReturnVC()
+            rentalVC.nowFun = "Rental"
+            self.navigationController?.pushViewController(rentalVC, animated: true)
+            }
+            
+        button.addAction(goToRentalVCAction, for: .touchUpInside)
+        
+        return button
+    }()
+    
+    lazy var returnButton: UIButton = {
+        let button = UIButton()
+        
+        button.setTitle("반납", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.setDimensions(height: 54, width: 126)
+        button.layer.cornerRadius = 20
+        button.backgroundColor = UIColor(named: "main")
+        
+        let goToReturnVCAction = UIAction { _ in
+            let returnVC = Rental_ReturnVC()
+            returnVC.nowFun = "Return"
+            self.navigationController?.pushViewController(returnVC, animated: true)
+            }
+            
+        button.addAction(goToReturnVCAction, for: .touchUpInside)
+        
+        return button
+    }()
+    
+    lazy var buttonHStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [self.rentalButton, self.returnButton])
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.alignment = .center
+        stackView.distribution = .equalSpacing
+        stackView.spacing = 31
+
+        return stackView
+    }()
+    
+    
+    // MARK: - Lifecycle
+    // 생명주기와 관련된 메서드 (viewDidLoad, viewDidDisappear...)
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureUI()
     }
-    */
+    
+    // MARK: - Actions
+    // IBAction 및 사용자 인터랙션과 관련된 메서드 정의
+    func configureUI() {
+//        view.backgroundColor = .white
+        view.backgroundColor = UIColor(named: "main")
+        
+        view.addSubview(searchbar)
+        view.addSubview(introView)
+        introView.addSubview(vStackView)
+        introView.addSubview(findLoadButton)
+        introView.addSubview(umbrellaHStackView)
+        introView.addSubview(buttonHStackView)
+        setupToolbar()
+        
+
+        searchbar.anchor(top:view.topAnchor, paddingTop: 10)
+        searchbar.centerX(inView: view)
+        introView.anchor(left: view.leftAnchor, bottom: toolbar.topAnchor)
+        vStackView.anchor(top: introView.topAnchor, paddingTop: 41)
+        vStackView.centerX(inView: introView)
+        findLoadButton.anchor(top: introView.topAnchor, right: introView.rightAnchor, paddingTop: 37, paddingRight: 40)
+        umbrellaHStackView.anchor(top: vStackView.bottomAnchor, paddingTop: 38)
+        umbrellaHStackView.centerX(inView: introView)
+        buttonHStackView.anchor(top: umbrellaHStackView.bottomAnchor, paddingTop: 34)
+        buttonHStackView.centerX(inView: introView)
+    }
+    
+    func setupToolbar() {
+        toolbar.barTintColor = UIColor(named: "white")
+        view.addSubview(toolbar)
+        toolbar.anchor(left: view.leftAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, right: view.rightAnchor)
+        toolbar.setItems(toolItems, animated: false)
+    }
+    
+    // MARK: - Helpers
+    // 설정, 데이터처리 등 액션 외의 메서드를 정의
+    
 
 }
