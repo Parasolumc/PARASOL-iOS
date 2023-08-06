ac53c4a5ef8dccc1ee77ae1807220b2f497f6634
imdanaa <jangs1920@gmail.com>
2023-08-06 16:42:49 +0900

    [feature5] 사장님 홈 화면 구성

--- PARASOL/PARASOL/Scenes/Home/OwnerHome/OwnerVC.swift
+++ PARASOL/PARASOL/Scenes/Home/OwnerHome/OwnerVC.swift
@@ -9,21 +9,302 @@ import UIKit
 
 class OwnerVC: UIViewController {
 
+    // MARK: - Properties
+    // 변수 및 상수, IBOutlet
+    // 화면 사이즈
+    var bounds = UIScreen.main.bounds
+    lazy var screenWidth = bounds.size.width //화면 너비
+    lazy var screenHeight = bounds.size.height //화면 높이
+    
+    // 대여가능한 우산의 개수
+    var umbrellaNum = 9
+    
+    // MARK: [UI components]
+    var nameLabel: UILabel = {
+        let label = UILabel()
+        
+        label.text = "상점명"
+        label.font = .boldSystemFont(ofSize: 24)
+        label.textColor = .black
+        return label
+    }()
+    
+    var addressLabel: UILabel = {
+        let label = UILabel()
+        
+        label.text = "서울 서대문구 이화여대길 77"
+        label.font = .systemFont(ofSize: 16)
+        label.textColor = .black
+        return label
+    }()
+    
+    lazy var vStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [self.nameLabel, self.addressLabel])
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.alignment = .leading
+        stackView.distribution = .fillProportionally
+        stackView.spacing = 8
+        
+        stackView.setDimensions(height: 100, width: (screenWidth - 50))
+        return stackView
+    }()
+    
+    // 대여 가능 우산 개수 관련 요소들 생성
+    var tag1Label: UILabel = {
+        var label = UILabel()
+        
+        label.text = "대여가능 우산"
+        label.font = .boldSystemFont(ofSize: 20)
+        label.textColor = .black
+        return label
+    }()
+    
+    let umbrellaImage: UIImageView = {
+            let imageView = UIImageView()
+        
+            imageView.image = UIImage(named: "umbrella")
+        
+            return imageView
+    }()
+    
+    lazy var tag2Label: UILabel = {
+        var label = UILabel()
+        
+        label.text = ": " + String(self.umbrellaNum) + "개"
+        label.font = .boldSystemFont(ofSize: 20)
+        label.textColor = .black
+        return label
+    }()
+    
+    lazy var umbrellaHStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [self.tag1Label, self.umbrellaImage, self.tag2Label])
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.alignment = .center
+        stackView.distribution = .equalSpacing
+        stackView.spacing = 10
+
+        return stackView
+    }()
+    
+    // 대여/반납/구매 버튼
+    let rentLabel1: UILabel = {
+        var label = UILabel()
+        
+        label.text = "손님에게"
+        label.font = .systemFont(ofSize: 16)
+        label.textColor = .black
+        
+        return label
+    }()
+    
+    let rentLabel2: UILabel = {
+        var label = UILabel()
+        
+        label.text = "우산을"
+        label.font = .systemFont(ofSize: 16)
+        label.textColor = .black
+        
+        return label
+    }()
+    
+    let rentLabel3: UILabel = {
+        var label = UILabel()
+        
+        label.text = "빌려줄래요"
+        label.font = .boldSystemFont(ofSize: 20)
+        label.textColor = .black
+        
+        return label
+    }()
+    
+    lazy var rentLabelVStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [self.rentLabel1, self.rentLabel2, self.rentLabel3])
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.alignment = .leading
+        stackView.distribution = .fillProportionally
+        stackView.spacing = 5
+        stackView.backgroundColor = .clear
+        
+        return stackView
+    }()
+    
+    lazy var rentButton: UIView = {
+       let view = UIView()
+        
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.widthAnchor.constraint(equalToConstant: 159).isActive = true
+        view.heightAnchor.constraint(equalToConstant: 156).isActive = true
+        view.layer.cornerRadius = 30
+        view.backgroundColor = UIColor(named: "light")
+        view.addSubview(rentLabelVStackView)
+        rentLabelVStackView.centerX(inView: view)
+        rentLabelVStackView.centerY(inView: view)
+        
+        view.isUserInteractionEnabled = true
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(goToOwnerRentVC))
+        view.addGestureRecognizer(tapGesture)
+        
+        return view
+    }()
+    
+    let returnLabel1: UILabel = {
+        var label = UILabel()
+        
+        label.text = "손님이"
+        label.font = .systemFont(ofSize: 16)
+        label.textColor = .black
+        
+        return label
+    }()
+    
+    let returnLabel2: UILabel = {
+        var label = UILabel()
+        
+        label.text = "우산을"
+        label.font = .systemFont(ofSize: 16)
+        label.textColor = .black
+        
+        return label
+    }()
+    
+    let returnLabel3: UILabel = {
+        var label = UILabel()
+        
+        label.text = "반납했어요"
+        label.font = .boldSystemFont(ofSize: 20)
+        label.textColor = .black
+        
+        return label
+    }()
+    
+    lazy var returnLabelVStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [self.returnLabel1, self.returnLabel2, self.returnLabel3])
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.alignment = .leading
+        stackView.distribution = .fillProportionally
+        stackView.spacing = 5
+        stackView.backgroundColor = .clear
+        
+        return stackView
+    }()
+    
+    lazy var returnButton: UIView = {
+       let view = UIView()
+        
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.widthAnchor.constraint(equalToConstant: 159).isActive = true
+        view.heightAnchor.constraint(equalToConstant: 156).isActive = true
+        view.layer.cornerRadius = 30
+        view.backgroundColor = UIColor(named: "light")
+        view.addSubview(returnLabelVStackView)
+        returnLabelVStackView.centerX(inView: view)
+        returnLabelVStackView.centerY(inView: view)
+        
+        view.isUserInteractionEnabled = true
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(goToOwnerReturnVC))
+        view.addGestureRecognizer(tapGesture)
+        
+        return view
+    }()
+    
+    lazy var buttonHStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [self.rentButton, self.returnButton])
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.alignment = .center
+        stackView.distribution = .equalSpacing
+        stackView.spacing = 24
+
+        return stackView
+    }()
+    
+    let buyLabel: UILabel = {
+        var label = UILabel()
+        
+        label.text = "손님에게 중고 우산을 구매해요"
+        label.font = .systemFont(ofSize: 16)
+        label.textColor = .black
+        
+        return label
+    }()
+    
+    lazy var buyButton: UIView = {
+       let view = UIView()
+        
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.widthAnchor.constraint(equalToConstant: 342).isActive = true
+        view.heightAnchor.constraint(equalToConstant: 69).isActive = true
+        view.layer.cornerRadius = 20
+        view.backgroundColor = UIColor(named: "main")
+        view.addSubview(buyLabel)
+        buyLabel.centerX(inView: view)
+        buyLabel.centerY(inView: view)
+        
+        view.isUserInteractionEnabled = true
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(goToOwnerBuyVC))
+        view.addGestureRecognizer(tapGesture)
+        
+        return view
+    }()
+    
+    // MARK: - Lifecycle
+    // 생명주기와 관련된 메서드 (viewDidLoad, viewDidDisappear...)
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        configureUI()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Actions
+    // IBAction 및 사용자 인터랙션과 관련된 메서드 정의
+    func configureUI() {
+        view.backgroundColor = .white
+        view.addSubview(vStackView)
+        view.addSubview(umbrellaHStackView)
+        view.addSubview(buttonHStackView)
+        view.addSubview(buyButton)
+        
+        vStackView.anchor(top: view.topAnchor, paddingTop: 65)
+        vStackView.centerX(inView: view)
+        umbrellaHStackView.anchor(top: vStackView.bottomAnchor, paddingTop: 50)
+        umbrellaHStackView.centerX(inView: view)
+        buttonHStackView.anchor(top: umbrellaHStackView.bottomAnchor, paddingTop: 65)
+        buttonHStackView.centerX(inView: view)
+        buyButton.anchor(top: buttonHStackView.bottomAnchor, paddingTop: 15)
+        buyButton.centerX(inView: view)
+        
     }
-    */
+    
+    // 사장님 Rent 페이지로 이동
+    @objc func goToOwnerRentVC() {
+        let rentVC = Owner_Rental_ReturnVC()
+        rentVC.nowFun = "Rental"
+        self.navigationController?.pushViewController(rentVC, animated: true)
+    }
+    
+    // 사장님 Return 페이지로 이동
+    @objc func goToOwnerReturnVC() {
+        let returnVC = Owner_Rental_ReturnVC()
+        returnVC.nowFun = "Return"
+        self.navigationController?.pushViewController(returnVC, animated: true)
+    }
+    
+    // 사장님 Buy 페이지로 이동
+    @objc func goToOwnerBuyVC() {
+        let buyVC = Owner_Rental_ReturnVC()
+        buyVC.nowFun = "Buy"
+        self.navigationController?.pushViewController(buyVC, animated: true)
+    }
+    // MARK: - Helpers
+    // 설정, 데이터처리 등 액션 외의 메서드를 정의
 
 }
--- PARASOL/PARASOL/Scenes/Search/SearchVC.swift
+++ PARASOL/PARASOL/Scenes/Search/SearchVC.swift
@@ -12,8 +12,24 @@ private let cellID = "SearchCell"
 class SearchVC: UIViewController{
     // MARK: - Properties
     // 변수 및 상수, IBOutlet
-
+    var searchText:String = ""
+    
     // MARK: [UI components]
+    let searchBar: UISearchBar = {
+        let searchbar = UISearchBar()
+        searchbar.placeholder = "검색"
+        searchbar.tintColor = .blue
+        searchbar.setImage(UIImage(named: "search"), for: UISearchBar.Icon.search, state: .normal)
+        searchbar.setImage(UIImage(named: "icCancel"), for: .clear, state: .normal)
+        // 서치바에 그림자 추가
+        searchbar.layer.shadowColor = UIColor.gray.cgColor
+        searchbar.layer.shadowOpacity = 0.3
+        searchbar.layer.shadowOffset = CGSize(width: 0, height: 3)
+        searchbar.layer.shadowRadius = 2
+        
+        return searchbar
+    }()
+    
     let searchTableView = UITableView()
     
     // MARK: - Lifecycle
@@ -22,7 +38,13 @@ class SearchVC: UIViewController{
         super.viewDidLoad()
         
         configureUI()
+        setNavigationBar()
     }
+//    override func viewDidDisappear(_ animated: Bool) {
+//        super.viewDidDisappear(animated)
+//
+//        navigationController?.navigationBar.isTranslucent = true
+//    }
     
     // MARK: - Actions
     // IBAction 및 사용자 인터랙션과 관련된 메서드 정의
@@ -35,6 +57,71 @@ class SearchVC: UIViewController{
     
         view.addSubview(searchTableView)
         searchTableView.anchor(top: view.topAnchor, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor)
+    }
+    
+    func setNavigationBar() {
+        navigationController?.isNavigationBarHidden = false
+        navigationController?.navigationBar.tintColor = UIColor(named: "white")
+        navigationController?.navigationBar.isTranslucent = true
+        
+        // 서치바 넣기
+        self.navigationController?.navigationBar.topItem?.titleView = searchBar
+              
+        // 서치바 커스텀
+        if let textfield = searchBar.value(forKey: "searchField") as? UITextField {
+            //서치바 백그라운드 컬러
+            textfield.backgroundColor = UIColor(named: "white")
+            //플레이스홀더 글씨 색 정하기
+//            textfield.attributedPlaceholder = NSAttributedString(string: textfield.placeholder ?? "", attributes: [NSAttributedString.Key.foregroundColor : UIColor.lightGray])
+            //서치바 텍스트입력시 색 정하기
+            textfield.textColor = UIColor(named: "black")
+            textfield.layer.cornerRadius = 18
+            textfield.clipsToBounds = true
+        }
+        
+        // 서치바 검색어 세팅
+        if searchText != "" {
+            setSearchText(_searchText: searchText)
+        }
+        
+        // 네비게이션 바 아이템 넣기
+        setNCRB()
+    }
+    
+    // 네비게이션 바 아이템 세팅
+    func setNCRB() {
+        // left bar item
+        let leftCustomView = UIView(frame: CGRect(x: 0, y: 0, width: 25.0, height: 25.0))
+        
+        let navigatorArrow: UIImageView = {
+            let imageView = UIImageView()
+            
+            imageView.image = UIImage(named: "navigator")
+            
+            imageView.contentMode = .scaleToFill
+            imageView.setDimensions(height: 25.0, width: 25.0)
+            
+            imageView.isUserInteractionEnabled = true
+            let tapGesture = UITapGestureRecognizer(target: self, action: #selector(goToUserFunc))
+            imageView.addGestureRecognizer(tapGesture)
+            
+            return imageView
+        }()
+        
+        leftCustomView.addSubview(navigatorArrow)
+        navigatorArrow.translatesAutoresizingMaskIntoConstraints = false
+        
+        let leftBarItem = UIBarButtonItem(customView: leftCustomView)
+        self.navigationItem.leftBarButtonItem = leftBarItem
+    }
+    
+    func setSearchText(_searchText: String) {
+        searchBar.text = searchText
+    }
+    
+    @objc func goToUserFunc() {
+        let vc = UserTabBarVC()
+        (UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate)?.changeRootVC(vc, animated: false)
     }
     
     // MARK: - Helpers
