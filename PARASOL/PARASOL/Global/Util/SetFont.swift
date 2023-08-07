ed03daba8ab3f5d0ab660beb7f137ce8827506cf
imdanaa <jangs1920@gmail.com>
2023-08-07 17:21:40 +0900

    [Add] 커스텀 폰트 추가

--- PARASOL/PARASOL/Global/Util/SetFont.swift
+++ PARASOL/PARASOL/Global/Util/SetFont.swift
@@ -5,4 +5,27 @@
 //  Created by 장윤정 on 2023/07/17.
 //
 
-import Foundation
+import UIKit
+
+enum BasicFont: String {
+    case light = "Pretendard-Light"
+    case regular = "Pretendard-Regular"
+    case medium = "Pretendard-Medium"
+    case semibold = "Pretendard-SemiBold"
+    case bold = "Pretendard-Bold" 
+}
+extension UIFont {
+    // bold
+    static let B14 = UIFont(name: BasicFont.bold.rawValue, size: 14)
+    static let B16 = UIFont(name: BasicFont.bold.rawValue, size: 16)
+    static let B18 = UIFont(name: BasicFont.bold.rawValue, size: 18)
+    // semibold
+    static let SB16 = UIFont(name: BasicFont.semibold.rawValue, size: 16)
+    // medium
+    static let M14 = UIFont(name: BasicFont.medium.rawValue, size: 14)
+    static let M16 = UIFont(name: BasicFont.medium.rawValue, size: 16)
+    static let M18 = UIFont(name: BasicFont.medium.rawValue, size: 18)
+    // regular
+    static let R14 = UIFont(name: BasicFont.regular.rawValue, size: 14)
+    static let R16 = UIFont(name: BasicFont.regular.rawValue, size: 16)
+}
